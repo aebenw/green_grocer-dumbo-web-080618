7b3fb26789a5d69cae843bf99253532f4bdf8fc7
Andrew Woodward <ebenwoodward@icloud.com>
2018-08-04 12:07:18 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -46,114 +46,16 @@ def apply_coupons(cart, coupons)
   end
 p cart
 end
-      
-      
-      
-      
-      
-  #     info[:count] -= amount
-  #     # 
-  #     cart["#{obj} W/COUPON"][:price] = worth
-  #     cart["#{obj} W/COUPON"][:clearance] = clear 
-  #     cart["#{obj} W/COUPON"][:count] = new_count
-  #     p cart
-  #     # cart = cart.merge(new_cart)
-  #   end
-  #   else cart[] 
-  # end
-  # cart.delete_if {|obj, info| info[:count] == 0}
-
-
-  
-  # cart.each do |item, info|
     
-    
-
-# p coupons
-#   x = []
-#   new_hash = {}
-#   coupons.reverse.each do |discount|
-#     discount.each do |k, v|
-#       x << v 
-#     end
-#   end
-
-# value = 0 
-# new_count = 0 
-#   cart.each do |name, info|
-#     info.each do |k, v|
-#       if name == x[0]
-        
-#         new_hash["#{name} W/COUPON"] = {:price => x[2]}
-#           if k == :count
-#           new_count = v % x[1]
-#           new_hash_count = v / x[1]
-#           new_hash["#{name} W/COUPON"][:count] = new_hash_count
-#           info[k] = new_count
-#           end
-#                 # binding.pry
-
-#           if k == :clearance
-#           new_hash["#{name} W/COUPON"][:clearance] = v 
-#           end
-#         end
-#         # binding.pry
-#           if x.length > 3
-#           x.delete!(0..2)
-#           end
-#       end
-#     end
-# merged = cart.merge(new_hash)
-
-
-# merged.each do |name, info|
-#   info.each do |k, v|
-#     if info[:count] == 0 
-#       merged.delete(name)
-#     end
-#   end
-# end
-# p merged
-
-
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |obj, info|
+    info.each do |k, v|
+    if info[:clearance] == true 
+      info[:price] = info[:price] 
 end
 
 def checkout(cart, coupons)
   # code here
 end
-
-# def consolidate_cart(cart)
-#   count = Hash.new(0)
-#   cart.each do |item|
-#       item.each do |name, info|
-#           count[name] += 1
-#       end
-#     end
-    
-#   cart.each do |item|
-#       item.find do |name, info|
-#         count.find do |kind, number|
-#           if name == kind 
-#             info[:count] = number
-#           end
-#         end
-#       end
-#     end
-#     p cart
-# end
-
-# def apply_coupons(cart, coupons)
-#   # code here
-# end
-
-# def apply_clearance(cart)
-#   # code here
-# end
-
-# def checkout(cart, coupons)
-#   # code here
-# end
 
